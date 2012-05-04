ee88088a3f8533676a3f0c10a2d316d6d561a9b7
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/RollupPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/RollupPlugin.pm
+++ lib/TWiki/Plugins/RollupPlugin.pm
@@ -9,7 +9,7 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
+# GNU General Public License for more details, published at
 # http://www.gnu.org/copyleft/gpl.html
 #
 # =========================
@@ -26,25 +26,24 @@
 #   insidePREHandler     ( $text )
 #   endRenderingHandler  ( $text )
 #
-# initPlugin is required, all other are optional. 
+# initPlugin is required, all other are optional.
 # For increased performance, all handlers except initPlugin are
 # disabled. To enable a handler remove the leading DISABLE_ from
 # the function name.
-# 
+#
 # NOTE: To interact with TWiki use the official TWiki functions
 # in the &TWiki::Func module. Do not reference any functions or
 # variables elsewhere in TWiki!!
 
-
 # =========================
-package TWiki::Plugins::RollupPlugin; 	
+package TWiki::Plugins::RollupPlugin;
 
 # =========================
 use vars qw(
-        $web $topic $user $installWeb $VERSION $RELEASE $debug
-        $exampleCfgVar $totalEstimatedTime $totalActualTime
-	$totalRemainingTime
-    );
+  $web $topic $user $installWeb $VERSION $RELEASE $debug
+  $exampleCfgVar $totalEstimatedTime $totalActualTime
+  $totalRemainingTime
+);
 
 # This should always be $Rev$ so that TWiki can determine the checked-in
 # status of the plugin. It is used by the build automation tools, so
@@ -56,28 +55,27 @@ $VERSION = '$Rev$';
 # of the version number in PLUGINDESCRIPTIONS.
 $RELEASE = 'Dakar';
 
-
 # =========================
-sub initPlugin
-{
+sub initPlugin {
     ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $TWiki::Plugins::VERSION < 1 ) {
-        &TWiki::Func::writeWarning( "Version mismatch between EmptyPlugin and Plugins.pm" );
+    if ( $TWiki::Plugins::VERSION < 1 ) {
+        &TWiki::Func::writeWarning(
+            "Version mismatch between EmptyPlugin and Plugins.pm");
         return 0;
     }
 
     # Plugin correctly initialized
-    &TWiki::Func::writeDebug( "- TWiki::Plugins::RollupPlugin::initPlugin( $web.$topic ) is OK" ) if $debug;
-
+    &TWiki::Func::writeDebug(
+        "- TWiki::Plugins::RollupPlugin::initPlugin( $web.$topic ) is OK")
+      if $debug;
 
     return 1;
 }
 
 # =========================
-sub commonTagsHandler
-{
+sub commonTagsHandler {
     $_[0] =~ s/%ROLLUP{(.*?)}%/&rollup($1)/geo;
     $_[0] =~ s/%ROLLUP%/&rollup()/geo;
 }
@@ -88,56 +86,60 @@ sub commonTagsHandler
 #==========================#
 
 #=================================================
-# Identifies the topic to rollup and 
+# Identifies the topic to rollup and
 # does just that.
 #=================================================
-sub rollup
-{
+sub rollup {
     my ($tempTopic) = @_;
-    
+
     $tempTopic = $topic if $tempTopic eq "";
 
-
-    my ($topicToSearch) = concatenateStringArray(&TWiki::Func::readTopic($web, $tempTopic));
-    my ($returnString) = "---++ Rollup of <nop>".$tempTopic.":\n---\n";
-    $returnString .= getStories($topicToSearch)."\n";
+    my ($topicToSearch) =
+      concatenateStringArray( &TWiki::Func::readTopic( $web, $tempTopic ) );
+    my ($returnString) = "---++ Rollup of <nop>" . $tempTopic . ":\n---\n";
+    $returnString .= getStories($topicToSearch) . "\n";
     $returnString .= "---+++ Total For Iteration \n";
-    $returnString .= "|*All Tasks*| *Estimated Time* | *Actual Time* | *Remaining Time* |\n";
-    $returnString .= "|All|  ".$totalEstimatedTime." |  ".$totalActualTime." |  ".$totalRemainingTime." |\n";
+    $returnString .=
+      "|*All Tasks*| *Estimated Time* | *Actual Time* | *Remaining Time* |\n";
+    $returnString .=
+        "|All|  "
+      . $totalEstimatedTime . " |  "
+      . $totalActualTime . " |  "
+      . $totalRemainingTime . " |\n";
 
 }
 
 #=================================================
-# Finds the stories in the given topic. Iterates 
+# Finds the stories in the given topic. Iterates
 # through them to find tasks.
 #=================================================
-sub getStories
-{
+sub getStories {
     my ($topicToSearch) = @_;
-    my($returnString); 
-    my (@stories) = findTopicWithSuffix($topicToSearch, "Story");
+    my ($returnString);
+    my (@stories) = findTopicWithSuffix( $topicToSearch, "Story" );
     foreach $story (@stories) {
-	my ($storyString) = concatenateStringArray(&TWiki::Func::readTopic($web, $story));
-	$returnString .= "---++++ <nop>".$story."\n";
-	$returnString .= "_Developer:".getDeveloper($storyString)."_\n\n";
-	$returnString .= getTasks($storyString);
+        my ($storyString) =
+          concatenateStringArray( &TWiki::Func::readTopic( $web, $story ) );
+        $returnString .= "---++++ <nop>" . $story . "\n";
+        $returnString .= "_Developer:" . getDeveloper($storyString) . "_\n\n";
+        $returnString .= getTasks($storyString);
     }
     $returnString .= "---\n";
 
     # Might have tasks in story
-    my($iterationTasks) = getTasks($topicToSearch);
-    $returnString .= "---++++ Iteration Tasks \n\n".$iterationTasks if($iterationTasks ne "");
+    my ($iterationTasks) = getTasks($topicToSearch);
+    $returnString .= "---++++ Iteration Tasks \n\n" . $iterationTasks
+      if ( $iterationTasks ne "" );
     $returnString .= "---";
     return $returnString;
 }
 
 #=================================================
-# Finds the the entry Developer: 
+# Finds the the entry Developer:
 # and returns what is entered after it
 #=================================================
-sub getDeveloper
-{
-    my($topicToSearch) = @_;
+sub getDeveloper {
+    my ($topicToSearch) = @_;
     $topicToSearch =~ /(.*?)\b(.*Developer:\D?)\b(\w*)/g;
     return $3;
 }
@@ -145,42 +147,53 @@ sub getDeveloper
 #=================================================
 # Finds the tasks in a story
 #=================================================
-sub getTasks
-{
+sub getTasks {
     my ($topicToSearch) = @_;
-    my ($returnString) =  "| *Task* | *Estimated Time* | *Actual Time* | *Remaining Time* |\n ";
-    my (@tasks) = findTopicWithSuffix($topicToSearch, "Task");
+    my ($returnString) =
+      "| *Task* | *Estimated Time* | *Actual Time* | *Remaining Time* |\n ";
+    my (@tasks) = findTopicWithSuffix( $topicToSearch, "Task" );
 
     my ($sumEstimatedTime);
     my ($sumActualTime);
     my ($sumRemainingTime);
     foreach $task (@tasks) {
-	my ($topicToSearch) = concatenateStringArray(&TWiki::Func::readTopic($web,$task)); 
-	my $estimatedTime = findTime($topicToSearch, "EstimatedTime:");
-	my $actualTime = findTime($topicToSearch, "ActualTime:");
-	my $remainingTime = findTime($topicToSearch, "RemainingTime:");
-	$sumEstimatedTime += $estimatedTime;
-	$totalEstimatedTime += $estimatedTime;
-	$sumActualTime += $actualTime;
-	$totalActualTime += $actualTime;
-	$sumRemainingTime += $remainingTime;
-	$totalRemainingTime += $remainingTime;
-	$returnString .= "|<nop>".$task."|  ".$estimatedTime." |  ".$actualTime." |  ".$remainingTime." |\n";
+        my ($topicToSearch) =
+          concatenateStringArray( &TWiki::Func::readTopic( $web, $task ) );
+        my $estimatedTime = findTime( $topicToSearch, "EstimatedTime:" );
+        my $actualTime    = findTime( $topicToSearch, "ActualTime:" );
+        my $remainingTime = findTime( $topicToSearch, "RemainingTime:" );
+        $sumEstimatedTime   += $estimatedTime;
+        $totalEstimatedTime += $estimatedTime;
+        $sumActualTime      += $actualTime;
+        $totalActualTime    += $actualTime;
+        $sumRemainingTime   += $remainingTime;
+        $totalRemainingTime += $remainingTime;
+        $returnString .=
+            "|<nop>" 
+          . $task . "|  "
+          . $estimatedTime . " |  "
+          . $actualTime . " |  "
+          . $remainingTime . " |\n";
     }
-    $returnString .= "|*Sum:*|  *".$sumEstimatedTime."* |  *".$sumActualTime."* |  *".$sumRemainingTime."* |\n";  
-    return $returnString; 
+    $returnString .=
+        "|*Sum:*|  *"
+      . $sumEstimatedTime
+      . "* |  *"
+      . $sumActualTime
+      . "* |  *"
+      . $sumRemainingTime . "* |\n";
+    return $returnString;
 }
 
 #=================================================
 # Returns all the topics with a given suffix within a given topic.
 #=================================================
-sub findTopicWithSuffix 
-{
-    my ($topicTo, $suffix) = @_;
+sub findTopicWithSuffix {
+    my ( $topicTo, $suffix ) = @_;
     my (@topics);
-    while ($topicTo =~ /(.*?)\b(.*$suffix)\b/gm) {
-	$topics[++$#topics] = $2;
-	$topicNo++; 
+    while ( $topicTo =~ /(.*?)\b(.*$suffix)\b/gm ) {
+        $topics[ ++$#topics ] = $2;
+        $topicNo++;
     }
     return @topics;
 }
@@ -188,12 +201,11 @@ sub findTopicWithSuffix
 #=================================================
 # Finds numbered value after a given entry
 #=================================================
-sub findTime 
-{
-    my($topicToSearch, $timeToFind) = @_;
+sub findTime {
+    my ( $topicToSearch, $timeToFind ) = @_;
     $topicToSearch =~ /(.*?)\b($timeToFind\D*?)\b(\d*)/g;
     return $3;
-	
+
 }
 
 #=================================================
@@ -202,14 +214,10 @@ sub findTime
 sub concatenateStringArray {
     my ($concatenatedString);
     foreach $part (@_) {
-	$concatenatedString .= $part; 
+        $concatenatedString .= $part;
     }
     return $concatenatedString;
 }
 
-
 1;
-
-
-
 
